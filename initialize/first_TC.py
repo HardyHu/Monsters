5c4c2baa6d9a75d3a7d085c50b5a8f9a00ead22b
HardyHu <huzhikai11@sina.com>
2018-10-15 19:29:30 +0800

    Here I add author name to this file and i made some comment to this~~2018.10.15

--- initialize/first_TC.py
+++ initialize/first_TC.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+__author__ = 'Hardy'
+'will add TestSuite/TestSkip & TestCase/TestLoader/TestReporter in the near future'
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
